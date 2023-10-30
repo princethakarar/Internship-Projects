c794420da21e6bea3fd9dcd99b215ec2eb49bbc2
Prince Thakarar <113857771+princethakarar@users.noreply.github.com>
2023-10-30 19:16:28 +0530

    Add files via upload

---
+++ portfolio website/script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav a');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 2) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').slice(1) === current) {
+            link.classList.add('active');
+        }
+    });
+});
